7cc9eb00566ecc07228cd13c661fd71989ca3b57
yassineaboub <yassineaboub@live.fr>
2020-02-19 08:40:17 +0000

    Git fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
-import datetime
+from datetime import datetime
 from uuid import uuid4
 
+time = '%Y-%m-%dT%H:%M:%S.%f'
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-
+    def __init__(self, *args, **kwargs):
+        for k, v in kwargs.items():
+            if k == 'id':
+                self.id = v
+            elif k == 'created_at':
+                    self.created_at = datetime.strptime(v, time)
+            elif k == 'updated_at':
+                    self.updated_at = datetime.strptime(v, time)
+                    setattr(self, k, v)
+        else:
+                self.id = str(uuid4())
+                self.created_at = datetime.now()
+    
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__,
                 self.id, self.__dict__))
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         the_dict = self.__dict__
         the_dict ['__class__'] = self.__class__.__name__
-        self.created_at = datetime.datetime.now().isoformat()
-        self.updated_at = datetime.datetime.now().isoformat()
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
         return the_dict
